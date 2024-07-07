a1e969ae4b501152353e5d53423596c98b6c2e0b
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-08 00:27:42 +0200

    min_max

--- oddNumber.cpp
+++ oddNumber.cpp
@@ -4,20 +4,37 @@ using namespace std;
 
 
 int main(){
-    int number;
+    // int number;
 
-    cout<<"Enter a number : ";
-    cin>>number;
+    // cout<<"Enter a number : ";
+    // cin>>number;
 
-    if(number<0){
-        cout<<"Please dont enter negative number\n";
+    // if(number<0){
+    //     cout<<"Please dont enter negative number\n";
+    // }else{
+    //     if(number % 2 == 0){
+    //         cout<<number<<" is an even number"<<endl;
+    //     }else{
+    //         cout<<number <<" is an odd number"<<endl;
+    //     }
+    // }
+
+    int x,y,z,myMin, myMax;
+    cout<<"Enter three numbers : ";
+    cin>> x>>y>>z;
+    if(x>y && x>z){
+        myMax = x;
+        myMin = (y<z) ? y:z;
+    }else if(y>x && y>>z){
+        myMax =y;
+        myMin = (x<z) ? x:z;
     }else{
-        if(number % 2 == 0){
-            cout<<number<<" is an even number"<<endl;
-        }else{
-            cout<<number <<" is an odd number"<<endl;
-        }
+        myMax = z;
+        myMin = (x<y) ? x:y;
     }
+
+    cout<<"my maximum number is "<< myMax<< endl;
+    cout<<"my minumum number is "<< myMin<< endl;
 
 return 0;
 }
