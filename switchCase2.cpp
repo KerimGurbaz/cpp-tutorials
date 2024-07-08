610a53e6f132b697cefc425762528b45c585c189
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-08 11:56:55 +0200

    vowel or not

--- switchCase2.cpp
+++ switchCase2.cpp
@@ -3,23 +3,41 @@ using namespace std;
 
 int main(){
 
-    char myOperator;
-    float x,y;
-    cout<< "Choose un operator(+, -, *, /) : ";
-    cin>>myOperator;
-    cout<<"Enter two numbers : ";
-    cin>>x>>y;
+    // char myOperator;
+    // float x,y;
+    // cout<< "Choose un operator(+, -, *, /) : ";
+    // cin>>myOperator;
+    // cout<<"Enter two numbers : ";
+    // cin>>x>>y;
 
-    switch(myOperator){
-        case '+':cout<<"\nx + y = "<<x+y; break;
-        case '-':cout<<"\nx - y = "<<x-y; break;
-        case '*':cout<<"\nx * y = "<<x*y;break;
-        case '/':cout<<"\nx / y = "<<x/y;break;
+    // switch(myOperator){
+    //     case '+':cout<<"\nx + y = "<<x+y; break;
+    //     case '-':cout<<"\nx - y = "<<x-y; break;
+    //     case '*':cout<<"\nx * y = "<<x*y;break;
+    //     case '/':cout<<"\nx / y = "<<x/y;break;
 
-        default:cout<<"Wrong Choice\n";
+    //     default:cout<<"Wrong Choice\n";
 
 
-    }
+    // }
+
+    char letter;
+    cout<<"Enter a letter : ";
+    cin>>letter;
+ switch(letter){
+    case 'a':
+    case 'e':
+    case 'u':
+    case 'o':
+    case 'i':
+    cout<<letter<<" is a vowel \n";
+    break;
+    default:cout<<letter  <<" is a consonant";
+            cout<<" or not a letter "<<endl;break;
+
+
+
+ }
 
 
 
