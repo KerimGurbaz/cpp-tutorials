e76bd457ef4c82bb4b19121e5fd1905239ca1642
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-09 09:46:26 +0200

    while v-2

--- while.cpp
+++ while.cpp
@@ -2,12 +2,30 @@
 using namespace std;
 
 int main(){
-    int i = 1;
-    while(i<=7){
+    // int i = 1;
+    // while(i<=7){
 
-        cout<<"C++ is a wonderful language - "<<i<<endl;
-        i++;
+    //     cout<<"C++ is a wonderful language - "<<i<<endl;
+    //     i++;
 
+
+    // }
+
+    short int number(1);
+
+    while(number!=0){
+    cout<< "Please enter a number (1-10) : ";
+    cin>>number;
+    if(number<1 || number >10){
+        cout<<"Please enter between 1 and 10 "<<endl;
+
+    }else{
+        short int i(1) ;
+        while(i<=10 && number!=0){
+            cout<<i<< " X "<< number<<" = "<< i * number <<endl;
+            i++;
+        }
+    }
 
     }
 
