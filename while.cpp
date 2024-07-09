30e8c84a12489fb2a645322eb3a83672737823af
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-09 08:18:33 +0200

    while v-1

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+    while(i<=7){
+
+        cout<<"C++ is a wonderful language - "<<i<<endl;
+        i++;
+
+
+    }
+
+
+    return 0;
+}
