e671dde04fa849ad0ebe28b479701761c4325f34
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-10 17:11:04 +0200

    factorial v-2

--- factorial.cpp
+++ factorial.cpp
@@ -4,20 +4,28 @@ using namespace std;
 
 int main(){
 
-    int number, i,factorial = 1;
+    // int number, i,factorial = 1;
 
-    cout<<"Enter a number : ";
-    cin>>number;
+    // cout<<"Enter a number : ";
+    // cin>>number;
 
-    if(number<0){
-        cout<<"Please enter number greater then zero !";
-    }else{
-        for(i=1; i<=number; i++){
-            factorial = factorial*i;
-        }
-        cout<<number<<"! = "<<factorial<<"\n";
+    // if(number<0){
+    //     cout<<"Please enter number greater then zero !";
+    // }else{
+    //     for(i=1; i<=number; i++){
+    //         factorial = factorial*i;
+    //     }
+    //     cout<<number<<"! = "<<factorial<<"\n";
+    // }
+
+
+    short int i, j;
+    for(i=1; i<=4; i++){
+        for(j=1; j<=3; j++){
+            cout<<"Hello C++ ";
     }
-
+    cout<<"\n-------------------\n";
 
     return 0;
+}
 }
