af650dc522301576fc980eb18141286940baa732
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-11 16:39:39 +0200

    v-2

--- maxMin.cpp
+++ maxMin.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 
 
-int main(){
+#include <iostream>
+#include <limits.h> // INT_MAX ve INT_MIN için gerekli
+using namespace std;
 
-    int number;
+int main() {
+    int number, i = 1;
+    int smallestN = INT_MAX;
+    int largestN = INT_MIN;
 
-    cout<< INT_MAX<<endl;
-    cout<< INT_MIN<<endl;
+    do {
+        cout << i << "th number: ";
+        cin >> number;
+        if (number == 0) {
+            break;
+        }
+        if (number < smallestN) {
+            smallestN = number;
+        }
+        if (number > largestN) {
+            largestN = number;
+        }
+        i++;
+    } while (number != 0);
 
+    if (i == 1) {
+        cout << "No numbers entered." << endl;
+    } else {
+        cout << "Smallest number: " << smallestN << endl;
+        cout << "Largest number: " << largestN << endl;
+    }
 
     return 0;
 }
