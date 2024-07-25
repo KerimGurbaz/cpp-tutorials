367f6be144abb6bd7fc814f4d18a3e022dca3518
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-26 00:48:52 +0200

    v-2 for loop

--- hackerRank.cpp
+++ hackerRank.cpp
@@ -1,54 +1,98 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//    int a ,b;
+//    cin >>a;
+//    cin >>b;
+
+//     for (int i = a; i <= b; i++) {
+//         if (1 <= i && i <= 9) {
+//             switch (i) {
+//                 case 1:
+//                     cout << "one" << endl;
+//                     break;
+//                 case 2:
+//                     cout << "two" << endl;
+//                     break;
+//                 case 3:
+//                     cout << "three" << endl;
+//                     break;
+//                 case 4:
+//                     cout << "four" << endl;
+//                     break;
+//                 case 5:
+//                     cout << "five" << endl;
+//                     break;
+//                 case 6:
+//                     cout << "six" << endl;
+//                     break;
+//                 case 7:
+//                     cout << "seven" << endl;
+//                     break;
+//                 case 8:
+//                     cout << "eight" << endl;
+//                     break;
+//                 case 9:
+//                     cout << "nine" << endl;
+//                     break;
+//                 default:
+//                     break;
+//             }
+//         } else {
+//             if (i % 2 == 0) {
+//                 cout << "even" << endl;
+//             } else {
+//                 cout << "odd" << endl;
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 
 using namespace std;
 
+// Belirli bir sayının İngilizce karşılığını yazdıran fonksiyon
+void printNumberInWords(int num) {
+    switch (num) {
+        case 1: cout << "one" << endl; break;
+        case 2: cout << "two" << endl; break;
+        case 3: cout << "three" << endl; break;
+        case 4: cout << "four" << endl; break;
+        case 5: cout << "five" << endl; break;
+        case 6: cout << "six" << endl; break;
+        case 7: cout << "seven" << endl; break;
+        case 8: cout << "eight" << endl; break;
+        case 9: cout << "nine" << endl; break;
+        default: break;
+    }
+}
+
+// Sayının tek mi çift mi olduğunu yazdıran fonksiyon
+void printEvenOrOdd(int num) {
+    if (num % 2 == 0) {
+        cout << "even" << endl;
+    } else {
+        cout << "odd" << endl;
+    }
+}
+
 int main() {
-   int a ,b;
-   cin >>a;
-   cin >>b;
+    int a, b;
+    cin >> a;
+    cin >> b;
 
     for (int i = a; i <= b; i++) {
         if (1 <= i && i <= 9) {
-            switch (i) {
-                case 1:
-                    cout << "one" << endl;
-                    break;
-                case 2:
-                    cout << "two" << endl;
-                    break;
-                case 3:
-                    cout << "three" << endl;
-                    break;
-                case 4:
-                    cout << "four" << endl;
-                    break;
-                case 5:
-                    cout << "five" << endl;
-                    break;
-                case 6:
-                    cout << "six" << endl;
-                    break;
-                case 7:
-                    cout << "seven" << endl;
-                    break;
-                case 8:
-                    cout << "eight" << endl;
-                    break;
-                case 9:
-                    cout << "nine" << endl;
-                    break;
-                default:
-                    break;
-            }
+            printNumberInWords(i);
         } else {
-            if (i % 2 == 0) {
-                cout << "even" << endl;
-            } else {
-                cout << "odd" << endl;
-            }
+            printEvenOrOdd(i);
         }
     }
 
     return 0;
 }
-
