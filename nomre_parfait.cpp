d012767f81c51e800546a835b0dc0a0e95042187
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-29 14:42:36 +0200

    version-2

--- nomre_parfait.cpp
+++ nomre_parfait.cpp
@@ -1,22 +1,42 @@
 #include <iostream>
 using namespace std;
 
+// int main(){
+
+//     int number, i , sum =0;
+//     cout<<"Enter a number : ";
+//     cin>> number;
+//     for(i=1; i<=number/2; i++){
+//         if(number %i == 0)
+//             sum +=i;
+//     }
+//         if(sum == number){
+//             cout<<number<<" is a parfait number";
+//         }else{
+//             cout<<number<<" is not a parfait number";
+//         }
+
+//    return 0;
+
+// }
+
+//to find perfect numbers up to the entered number
 int main(){
 
-    int number, i , sum =0;
+    int number, i ,j, sum=0;
     cout<<"Enter a number : ";
     cin>> number;
-    for(i=1; i<=number/2; i++){
-        if(number %i == 0)
-            sum +=i;
-    }
-        if(sum == number){
-            cout<<number<<" is a parfait number";
-        }else{
-            cout<<number<<" is not a parfait number";
+    for(i=1; i<=number; i++){
+        sum=0;
+        for(j=1; j<=i/2; j++){
+             if(i %j == 0)
+            sum +=j;
         }
-
+       
+        if(sum == i){
+            cout<<i<<" is a parfait number"<<endl;
+        }
+    }
    return 0;
 
 }
-
