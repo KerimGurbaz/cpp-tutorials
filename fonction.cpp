0907ae6b4f7eab07ae69ad3bce4113777c49b043
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-07-31 12:59:23 +0200

    void v-1

---
+++ fonction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print(string fname);
+
+int main (){
+
+    print(" Kerim ");
+    print(" GURBAZ ");
+
+    return 0;
+}
+
+void print(string fname){
+    cout<<"Hello "<<fname<<"\n";
+}
