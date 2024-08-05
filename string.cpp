493ce59bb79a76de5d1a056597bc16edade65a68
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-05 17:40:01 +0200

    string v-3

--- string.cpp
+++ string.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
 
 int main(){
@@ -7,22 +8,28 @@ int main(){
     // char greeting[6] = {'H','e', 'l', 'l', 'o','\n'};
     // cout<<"message : " <<greeting<<endl;
 
-    char str1[20]="Kerim";
-    char str2[10]="GURBAZ";
-    char str3[20];
+    // char str1[20]="Kerim";
+    // char str2[10]="GURBAZ";
+    // char str3[20];
 
-    int mylength;
+    // int mylength;
 
-    strcpy(str3, str1);
-    cout<<"strcpy(str3, str1) :" <<str3<<endl;
+    // strcpy(str3, str1);
+    // cout<<"strcpy(str3, str1) :" <<str3<<endl;
 
-    strcat(str1,str2);
+    // strcat(str1,str2);
 
-     cout<<"strcpy(str1, str2) :" <<str1<<endl;
+    //  cout<<"strcpy(str1, str2) :" <<str1<<endl;
 
-     mylength = strlen(str1);
-      cout<<"strlen(str1)" <<mylength<<endl;
+    //  mylength = strlen(str1);
+    //   cout<<"strlen(str1)" <<mylength<<endl;
 
+    string myText = "life is hard isn't it";
+    string name = "Kerim, ";
+    string str3 ;
+    str3 = name.append(myText);
+    cout<<str3.append(3, '.')<<endl;
+ 
 
     return 0;
 }
