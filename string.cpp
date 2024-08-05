0ecaf44c2f3589c0405def7594b9b170432c93d6
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-05 17:26:48 +0200

    string v-2

--- string.cpp
+++ string.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main(){
 
-    char greeting[6] = {'H','e', 'l', 'l', 'o','\n'};
-    cout<<"message : " <<greeting<<endl;
+    // char greeting[6] = {'H','e', 'l', 'l', 'o','\n'};
+    // cout<<"message : " <<greeting<<endl;
+
+    char str1[20]="Kerim";
+    char str2[10]="GURBAZ";
+    char str3[20];
+
+    int mylength;
+
+    strcpy(str3, str1);
+    cout<<"strcpy(str3, str1) :" <<str3<<endl;
+
+    strcat(str1,str2);
+
+     cout<<"strcpy(str1, str2) :" <<str1<<endl;
+
+     mylength = strlen(str1);
+      cout<<"strlen(str1)" <<mylength<<endl;
 
 
     return 0;
