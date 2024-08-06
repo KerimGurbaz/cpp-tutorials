0a7d2dc78c425ec5d8b27d97ae06a616ddd6e8f0
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-06 16:24:47 +0200

    string2 v-1

---
+++ string2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    // string txt;
+    // cout<< "Enter a string : " ;
+    // getline (cin, txt);
+    // if(!txt.empty())
+    // cout<<"your text : "<<txt<<"\n";
+
+    // else
+    // cout<<"The string is empty\n";   
+
+
+    return 0;
+}
