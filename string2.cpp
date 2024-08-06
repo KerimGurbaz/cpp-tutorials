f3c8bd3fd73f616813d6e4691d5a9a0e579c25d5
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-06 16:46:51 +0200

    string comperation

--- string2.cpp
+++ string2.cpp
@@ -14,5 +14,24 @@ int main(){
     // cout<<"The string is empty\n";   
 
 
+    string txt1, txt2, txt3, largest , txt4(3,'.');
+
+
+    txt1 = "strawberry";
+    txt2 = "star";
+    txt3 = "soup";
+    if((txt1 >txt2) && (txt1 >txt3))
+        largest =txt1;
+
+    else if((txt2 >txt3) && (txt2 >txt3))
+
+    largest =txt2;
+
+    else
+    largest = txt3;
+
+    cout<<"largest : " << largest<<txt4<<" \n";
+
+
     return 0;
 }
