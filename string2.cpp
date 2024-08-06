95fb37b1db29f876326e6792273aa725b8b929d8
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-06 17:20:26 +0200

    str v-3

--- string2.cpp
+++ string2.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main(){
 
-    // string txt;
-    // cout<< "Enter a string : " ;
-    // getline (cin, txt);
-    // if(!txt.empty())
-    // cout<<"your text : "<<txt<<"\n";
+//     string txt;
+//     cout<< "Enter a string : " ;
+//     getline (cin, txt);
+//    for(int i=0; i<txt.size(); i++){
+//     if(isalnum(txt[i]))
+//     cout<<txt[i]<<" ";
+
+//    }
 
     // else
     // cout<<"The string is empty\n";   
 
 
-    string txt1, txt2, txt3, largest , txt4(3,'.');
+    // string txt1, txt2, txt3, largest , txt4(3,'.');
 
 
-    txt1 = "strawberry";
-    txt2 = "star";
-    txt3 = "soup";
-    if((txt1 >txt2) && (txt1 >txt3))
-        largest =txt1;
+    // txt1 = "strawberry";
+    // txt2 = "star";
+    // txt3 = "soup";
+    // if((txt1 >txt2) && (txt1 >txt3))
+    //     largest =txt1;
 
-    else if((txt2 >txt3) && (txt2 >txt3))
+    // else if((txt2 >txt3) && (txt2 >txt3))
 
-    largest =txt2;
+    // largest =txt2;
 
-    else
-    largest = txt3;
+    // else
+    // largest = txt3;
 
-    cout<<"largest : " << largest<<txt4<<" \n";
+    // cout<<"largest : " << largest<<txt4<<" \n"; 
 
 
     return 0;
