58cc8625c544dc970405a451831fb9d2a1ef48f0
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-08-08 00:47:01 +0200

    v2

--- sub_str.cpp
+++ sub_str.cpp
@@ -4,26 +4,29 @@ using namespace std;
 
 int main(){
 
-    // string txt = "Now step live, Where did you buy it ?? ";
-    // string txt2= txt.substr(5,7);
+    string txt = "Now step live, Where did you buy it ?? ";
+    string txt2= txt.substr(5,7);
 
-    // int i =  txt.find("did");
+    int i =  txt.find("did");
 
-    // cout<<txt2<<endl;
+    cout<<txt2<<endl;
 
-    // cout<<i<<endl;
+    cout<<i<<endl;
+    cout<<"merhaba: "<<9<<endl;
+    cout<<"selam : "<<0.33 * 24;
 
-    string password ="hope";
-    string enterCode;
 
-    cout<<"Please enter your password: ";
-    getline(cin, enterCode);
-    if(password.compare(enterCode)!=0)
-    cout<<"password isn't correct"<<endl;
-    else
-    cout<<"password is correct"<<endl;
+    // string password ="hope";
+    // string enterCode;
 
-    cout<<"hello world"<<endl;
+    // cout<<"Please enter your password: ";
+    // getline(cin, enterCode);
+    // if(password.compare(enterCode)!=0)
+    // cout<<"password isn't correct"<<endl;
+    // else
+    // cout<<"password is correct"<<endl;
+
+    // cout<<"hello world"<<endl;
 
 
 
