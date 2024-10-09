218b02e1f7bf79221fe146b6a4721dc3261e3023
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2024-10-10 00:43:42 +0200

    exercice

--- exercice_1.cpp
+++ exercice_1.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void f(const int& p1, const char& p2) {
-    cout << p1 << " , " << p2 << endl;
+int main(){
+
+for (int i = 1; i <= 5; ++i) {
+    switch (i) {
+        case 4 :
+            cout << "D";
+        case 2 :
+            cout << "B";
+            break;
+        default :
+            cout << "E";
+        case 3 :
+            cout << "C";
+            continue;
+        case 1 :
+            cout << "A";
+    }
+    cout << "F" << endl;
 }
 
-int main() {
-    const int ci = 65;
-    const char cc = 'B';
-    int vi = 67;
-    char vc = 'D';
-
-    f(vi, vc);    // Valide
-    f(vc, vi);    // Toujours une erreur, types incorrects
-    f(ci, vc);    // Valide
-    f(vi, cc);    // Valide
-    f(67, vc);     // Valide
-    f(vi, 'E');    // Valide
-    return 0;
 }
+
+
